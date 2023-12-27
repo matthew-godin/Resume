dc3d0560628e0bd4e262bbbd2ba45423d5ab07bf
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-12-27 17:34:02 -0500

    refactoring done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Github from './Github';
 import Skills from './Skills';
 import Experience from './Experience';
 import Education from './Education';
+import Projects from './Projects';
 
 const { Title, Paragraph } = Typography;
 
@@ -58,11 +59,18 @@ function App() {
     <div style={{ margin: '0', width: '100%', backgroundColor: '#242424'}}>
       <div style={{ paddingBottom: '2%', paddingLeft: '6%', paddingTop: '4%', marginTop: 0, marginBottom: 0, width: '95%'}}>
         <Title level={1} style={{marginTop: '0', marginBottom: '8px', color: 'white', fontWeight: '500'}}>Matthew Godin</Title>
-        <span><Website fontSize={headlineFontSize} /><Dot1 fontSize={headlineFontSize} /><Email fontSize={headlineFontSize} /><Dot2 fontSize={headlineFontSize} /><Linkedin fontSize={headlineFontSize} /><Dot2 fontSize={headlineFontSize} /><Github fontSize={headlineFontSize} /></span>
+        <span>
+          <Website fontSize={headlineFontSize} />
+          <Dot1 fontSize={headlineFontSize} />
+          <Email fontSize={headlineFontSize} />
+          <Dot2 fontSize={headlineFontSize} />
+          <Linkedin fontSize={headlineFontSize} />
+          <Dot2 fontSize={headlineFontSize} />
+          <Github fontSize={headlineFontSize} />
+        </span>
       </div>
     </div>
-    <div
-          style={{ paddingLeft: '6%', width: '89%'}}>
+    <div style={{ paddingLeft: '6%', width: '89%'}}>
         <Skills hrStyle={hrStyle} titleFontSize={titleFontSize} titleSecondaryPadding={titleSecondaryPadding} skillsPadding={skillsPadding}
           skillsPaddingLarger={skillsPaddingLarger} />
         <Experience hrStyle={hrStyle} mainPadding={mainPadding} secondaryPadding={secondaryPadding} secondarySecondaryPadding={secondarySecondaryPadding}
@@ -72,53 +80,8 @@ function App() {
         <Education hrStyle={hrStyle} titleFontSize={titleFontSize} titlePadding={titlePadding} titleSecondaryPadding={titleSecondaryPadding} mainPadding={mainPadding}
           secondaryPadding={secondaryPadding} universityTitleWeight={universityTitleWeight} universityDatesWeight={universityDatesWeight}
           jobDescriptionStyle={jobDescriptionStyle} universitySkillsWeight={universitySkillsWeight} universityCityStyle={universityCityStyle} />
-        <Title style={{ fontSize: titleFontSize, margin: '0', fontWeight: 'bold', color: 'black', paddingTop: titlePadding, paddingBottom: titleSecondaryPadding}} level={5}>Projects</Title>
-        <hr style={hrStyle}/>
-        <Row style={{paddingTop: mainPadding}}>
-          <Col style={projectTitleWeight} xs={12}><Link to='https://github.com/mathieu-godin/Licode-2023' style={projectTitleWeight}>Licode</Link></Col>
-          <Col xs={4}></Col>
-          <Col xs={8} style={projectDatesWeight}>September 2020 - April 2022</Col>
-        </Row>
-        <Row style={{paddingBottom: mainPadding}}>
-          <Col style={jobDescriptionStyle} xs={15}>Coding challenge platform with a 1v1 format (University of Waterloo capstone project)</Col>
-          <Col xs={9} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>TypeScript • Go • Deno • Python • Docker • SQL • React</Col>
-        </Row>
-        <Row style={{paddingTop: mainPadding}}>
-          <Col xs={12}><Link to='https://github.com/mathieu-godin/Talk-to-Me' style={projectTitleWeight}>Talk to Me</Link></Col>
-          <Col xs={4}></Col>
-          <Col xs={8} style={projectDatesWeight}>August 2022</Col>
-        </Row>
-        <Row style={{paddingBottom: mainPadding}}>
-          <Col style={jobDescriptionStyle} xs={18}>Chat bot app that simplifies filling in HR forms (first place, Ceridian Hackathon 2022)</Col>
-          <Col xs={6} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>TypeScript • Swift • iOS • React</Col>
-        </Row>
-        <Row style={{paddingTop: mainPadding}}>
-          <Col style={projectTitleWeight} xs={12}><Link to='https://github.com/mathieu-godin/OnceUponATime' style={projectTitleWeight}>Once Upon a Time</Link></Col>
-          <Col xs={4}></Col>
-          <Col xs={8} style={projectDatesWeight}>September 2018</Col>
-        </Row>
-        <Row style={{paddingBottom: mainPadding}}>
-          <Col style={jobDescriptionStyle} xs={16}>VR app generating scenery according to the story being told (first place, Hack the North 2018)</Col>
-          <Col xs={8} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>C# • Node.js • IBM Watson • Socket.io • Unity</Col>
-        </Row>
-        <Row style={{paddingTop: mainPadding}}>
-          <Col style={projectTitleWeight} xs={12}><Link to='https://github.com/mathieu-godin/Aya' style={projectTitleWeight}>Aya</Link></Col>
-          <Col xs={4}></Col>
-          <Col xs={8} style={projectDatesWeight}>September 2017</Col>
-        </Row>
-        <Row style={{paddingBottom: mainPadding}}>
-          <Col style={jobDescriptionStyle} xs={18}>Intelligent robot head capable of recognizing people and objects (first place, Hack the North 2017)</Col>
-          <Col xs={6} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>Python • AWS • Embedded Software</Col>
-        </Row>
-        <Row style={{paddingTop: mainPadding}}>
-          <Col style={projectTitleWeight} xs={12}><Link to='https://github.com/mathieu-godin/messenger-latex' style={projectTitleWeight}>Messenger Latex</Link></Col>
-          <Col xs={4}></Col>
-          <Col xs={8} style={projectDatesWeight}>June 2017</Col>
-        </Row>
-        <Row style={{paddingBottom: mainPadding}}>
-          <Col style={jobDescriptionStyle} xs={22}>Google Chrome plugin that allows writing mathematical expressions in Facebook Messenger (first place, MHacks Nano 2017)</Col>
-          <Col xs={2} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>JavaScript</Col>
-        </Row>
+        <Projects hrStyle={hrStyle} titleFontSize={titleFontSize} titlePadding={titlePadding} titleSecondaryPadding={titleSecondaryPadding} mainPadding={mainPadding}
+          projectTitleWeight={projectTitleWeight} projectDatesWeight={projectDatesWeight} />
       </div>
     </>
   );
---
+++ src/Projects.js
@@ -0,0 +1,58 @@
+import { Typography, Col, Row } from 'antd';
+const { Title, Paragraph } = Typography;
+
+function Projects(props) {
+  return (
+    <>
+        <Title style={{ fontSize: props.titleFontSize, margin: '0', fontWeight: 'bold', color: 'black', paddingTop: props.titlePadding, paddingBottom: props.titleSecondaryPadding}} level={5}>Projects</Title>
+        <hr style={props.hrStyle}/>
+        <Row style={{paddingTop: props.mainPadding}}>
+          <Col style={props.projectTitleWeight} xs={12}><Link to='https://github.com/matthew-godin/Licode-2023' style={props.projectTitleWeight}>Licode</Link></Col>
+          <Col xs={4}></Col>
+          <Col xs={8} style={props.projectDatesWeight}>September 2020 - April 2022</Col>
+        </Row>
+        <Row style={{paddingBottom: props.mainPadding}}>
+          <Col style={jobDescriptionStyle} xs={15}>Coding challenge platform with a 1v1 format (University of Waterloo capstone project)</Col>
+          <Col xs={9} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>TypeScript • Go • Deno • Python • Docker • SQL • React</Col>
+        </Row>
+        <Row style={{paddingTop: props.mainPadding}}>
+          <Col xs={12}><Link to='https://github.com/matthew-godin/Talk-to-Me' style={props.projectTitleWeight}>Talk to Me</Link></Col>
+          <Col xs={4}></Col>
+          <Col xs={8} style={props.projectDatesWeight}>August 2022</Col>
+        </Row>
+        <Row style={{paddingBottom: props.mainPadding}}>
+          <Col style={jobDescriptionStyle} xs={18}>Chat bot app that simplifies filling in HR forms (first place, Ceridian Hackathon 2022)</Col>
+          <Col xs={6} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>TypeScript • Swift • iOS • React</Col>
+        </Row>
+        <Row style={{paddingTop: props.mainPadding}}>
+          <Col style={props.projectTitleWeight} xs={12}><Link to='https://github.com/matthew-godin/OnceUponATime' style={props.projectTitleWeight}>Once Upon a Time</Link></Col>
+          <Col xs={4}></Col>
+          <Col xs={8} style={props.projectDatesWeight}>September 2018</Col>
+        </Row>
+        <Row style={{paddingBottom: props.mainPadding}}>
+          <Col style={jobDescriptionStyle} xs={16}>VR app generating scenery according to the story being told (first place, Hack the North 2018)</Col>
+          <Col xs={8} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>C# • Node.js • IBM Watson • Socket.io • Unity</Col>
+        </Row>
+        <Row style={{paddingTop: props.mainPadding}}>
+          <Col style={props.projectTitleWeight} xs={12}><Link to='https://github.com/matthew-godin/Aya' style={props.projectTitleWeight}>Aya</Link></Col>
+          <Col xs={4}></Col>
+          <Col xs={8} style={props.projectDatesWeight}>September 2017</Col>
+        </Row>
+        <Row style={{paddingBottom: props.mainPadding}}>
+          <Col style={jobDescriptionStyle} xs={18}>Intelligent robot head capable of recognizing people and objects (first place, Hack the North 2017)</Col>
+          <Col xs={6} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>Python • AWS • Embedded Software</Col>
+        </Row>
+        <Row style={{paddingTop: props.mainPadding}}>
+          <Col style={props.projectTitleWeight} xs={12}><Link to='https://github.com/matthew-godin/messenger-latex' style={props.projectTitleWeight}>Messenger Latex</Link></Col>
+          <Col xs={4}></Col>
+          <Col xs={8} style={props.projectDatesWeight}>June 2017</Col>
+        </Row>
+        <Row style={{paddingBottom: props.mainPadding}}>
+          <Col style={jobDescriptionStyle} xs={22}>Google Chrome plugin that allows writing mathematical expressions in Facebook Messenger (first place, MHacks Nano 2017)</Col>
+          <Col xs={2} style={{textAlign: 'right', fontSize: '11px', fontWeight: projectSkillsWeight}}>JavaScript</Col>
+        </Row>
+    </>
+  );
+}
+
+export default Projects;
