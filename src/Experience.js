2da42e2427e5c0aed1a8d323923162dfbb55ec98
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-01-14 18:07:48 -0500

    update

--- src/Experience.js
+++ src/Experience.js
@@ -22,7 +22,7 @@ function Experience(props) {
               <li style={props.jobAdditionalInformationStyle}>Developed LaunchDarkly APIs enabling the release of features to specific clients in a few clicks</li>
               <li style={props.jobAdditionalInformationStyle}>Revamped new and previous screens to follow the best accessibility standards thoroughly by conforming to WAI's WCAG 2.1</li>
               <li style={props.jobAdditionalInformationStyle}>Developed React grids and dialogs that fetch and send data to the back end</li>
-              <li style={props.jobAdditionalInformationStyle}>Developed an interactive dialog allowing the insertion of different tokens to send personalized reward messages</li>
+              <li style={props.jobAdditionalInformationStyle}>Developed interactive dialogs allowing the insertion of different tokens to send personalized reward messages</li>
             </ul>
           </Col>
         </Row>
