608ad554408a7e7da3149be153bcc26821794307
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-02-05 15:23:57 -0500

    update

--- update.ps1
+++ update.ps1
@@ -1,7 +1,7 @@
 rm .\matthew-godin-resume.pdf
 pm2 start .\node_modules\react-scripts\scripts\start.js
 while (!(Test-NetConnection localhost -Port 3000).TcpTestSucceeded) { Start-Sleep 1 }
-chrome --headless --print-to-pdf="$HOME\github\Resume\matthew-godin-resume.pdf" --no-pdf-header-footer http://${env:computername}:3000/
+chrome --headless --print-to-pdf="$HOME\Resume\matthew-godin-resume.pdf" --no-pdf-header-footer http://${env:computername}:3000/
 while (!(Test-Path .\matthew-godin-resume.pdf -PathType Leaf)) { Start-Sleep 1 }
 pm2 delete all
 git add .
