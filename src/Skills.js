a11038689d0d94e23840807d6c6a9d17f870bd9d
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-02-05 16:44:00 -0500

    update

--- src/Skills.js
+++ src/Skills.js
@@ -8,7 +8,7 @@ function Skills(props) {
         <hr style={props.hrStyle} />
         <Paragraph style={{ margin: '0', fontSize: '12px', fontWeight: 'bold', paddingTop: props.skillsPadding, paddingBottom: props.skillsPadding}}><Paragraph style={{ margin: '0', fontSize: '12px', display: 'inline', fontWeight: 'bold', color: 'black'}}>Programming Languages</Paragraph> - C# • TypeScript • JavaScript • SQL • Python • Go • C++ • C • Java • Swift • Kotlin • PHP</Paragraph>
         <Paragraph style={{ margin: '0', fontSize: '12px', fontWeight: 'bold', paddingTop: props.skillsPaddingLarger, paddingBottom: props.skillsPaddingLarger}}><Paragraph style={{ margin: '0', fontSize: '12px', display: 'inline', fontWeight: 'bold', color: 'black'}}>Tools</Paragraph> - Git • Bash • Powershell • Docker • VS Code • Visual Studio • IntelliJ • Android Studio • Xcode</Paragraph>
-        <Paragraph style={{ margin: '0', fontSize: '12px', fontWeight: 'bold', paddingTop: props.skillsPadding, paddingBottom: props.skillsPadding}}><Paragraph style={{ margin: '0', fontSize: '12px', display: 'inline', fontWeight: 'bold', color: 'black'}}>Frameworks</Paragraph> - Node.js • Deno • React • Vue • ASP.NET Core • Laravel • PyTorch • Kafka • Spark • WebGL • Jest • NUnit • JUnit</Paragraph>
+        <Paragraph style={{ margin: '0', fontSize: '12px', fontWeight: 'bold', paddingTop: props.skillsPadding, paddingBottom: props.skillsPadding}}><Paragraph style={{ margin: '0', fontSize: '12px', display: 'inline', fontWeight: 'bold', color: 'black'}}>Frameworks</Paragraph> - Node.js • Deno • React • Vue • ASP.NET • Laravel • PyTorch • Kafka • Spark • WebGL • Jest • NUnit • JUnit</Paragraph>
     </>
   );
 }
