e243dc8aa8eb57df48d0940181d3cc22b2e51f70
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-02-19 02:50:05 -0500

    update

--- src/Experience.js
+++ src/Experience.js
@@ -21,7 +21,7 @@ function Experience(props) {
               <li style={props.jobAdditionalInformationStyle}>Developed REST APIs and SQL Server queries performing sorts, filters, and business logic</li>
               <li style={props.jobAdditionalInformationStyle}>Developed LaunchDarkly APIs enabling the release of features to specific clients in a few clicks</li>
               <li style={props.jobAdditionalInformationStyle}>Revamped new and previous screens to follow the best accessibility standards thoroughly by conforming to WAI's WCAG 2.1</li>
-              <li style={props.jobAdditionalInformationStyle}>Developed React components including grids, toolbars and dialogs that fetch and send data to the back end</li>
+              <li style={props.jobAdditionalInformationStyle}>Developed React components including grids, toolbars and forms that fetch and send data to the back end</li>
               <li style={props.jobAdditionalInformationStyle}>Developed interactive dialogs to send personalized reward letter messages through the insertion of different template tokens</li>
             </ul>
           </Col>
