b4d260c02bc9142e9d87c1cc9e952b740210bb35
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-15 13:29:17 -0400

    update

--- src/Education.js
+++ src/Education.js
@@ -7,7 +7,7 @@ function Education(props) {
         <Title style={{ fontSize: props.titleFontSize, margin: '0', fontWeight: 'bold', color: 'black', paddingTop: props.titlePadding, paddingBottom: props.titleSecondaryPadding}} level={5}>Education</Title>
         <hr style={props.hrStyle}/>
         <Row style={{paddingTop: props.mainPadding, paddingBottom: props.secondaryPadding}}>
-          <Col style={props.universityTitleWeight} xs={12}>University of Waterloo <Paragraph style={props.universityCityStyle}>&ensp;Waterloo, ON</Paragraph></Col>
+          <Col style={props.universityTitleWeight} xs={12}>University of Waterloo</Col>
           <Col xs={4}></Col>
           <Col xs={8} style={props.universityDatesWeight}>September 2017 - April 2022</Col>
         </Row>
