66b5a11fce805088ba96ba2a4198c4a4d646a8d2
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-04-04 07:43:32 -0400

    update

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ function Project(props) {
     if (props.skills.length > 40) {
       descriptionProportioning = { description: 16, skills: 8 };
     } else if (props.skills.length > 30) {
-      descriptionProportioning = { description: 17, skills: 7 };
+      descriptionProportioning = { description: 19, skills: 5 };
     } else {
       descriptionProportioning = { description: 21, skills: 3 };
     }
